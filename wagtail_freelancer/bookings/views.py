271f0410fde915ec7aba16a9960c066e5c9805ce
Felix <felix.p.hall@gmail.com>
2019-02-28 18:15:58 +1000

    added email functionality.

--- wagtail_freelancer/bookings/views.py
+++ wagtail_freelancer/bookings/views.py
@@ -7,6 +7,7 @@ from rest_framework import status, filters, viewsets
 
 from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
+from django.core.mail import EmailMessage, EmailMultiAlternatives
 
 from bookings.models import Meeting
 from bookings.serializer import MeetingSerializer
@@ -29,6 +30,7 @@ class MeetingViewSet(viewsets.ModelViewSet):
 	# 		return JsonReponse(code=201, data=serializer.data)
 	# 	return JsonResponse(code=400, data="wrong parameters")
 	def partial_update(self, request, *args, **kwargs):
+
 		#print (request.data)
 		kwargs['partial'] = True
 		QuerySet = list(request.data)[0]
@@ -37,12 +39,30 @@ class MeetingViewSet(viewsets.ModelViewSet):
 		#print ('json: ' + type(json_data))
 		del query_data["id"]
 		del query_data['csrfmiddlewaretoken']
-
 		model_object = Meeting.objects.get(id=kwargs['pk'])
 		serializer = MeetingSerializer(model_object, data=query_data, partial=True)
 		#print (serializer)
+		print (query_data)
 		if serializer.is_valid(raise_exception=True):
 			serializer.save(id=kwargs['pk'], **serializer.validated_data)
+			date = model_object.day_time.strftime("%d/%m/%y %H:%M")
+			line1 = "Hey {}!\nThank you wanting to create something incredible.".format(query_data['name'])
+			line2 = " I cannot to see what we can achieve together."
+			line3 = " Here's a summary of what you've told me:"
+			line4 = "\nemail: {}\nphone number: {}\nMeetin day: {}".format(
+				query_data['email'], query_data['phone_number'], date)
+			line5 = "\nIf something comes up and for some reason you cannot make it, "
+			line6 = "please email me directly by replying to this email.\n\nLooking forwards to making something great!\nRegards,\nFelix Hall"
+			msg = line1 + line2 + line3 + line4 + line5 + line6 
+			email = EmailMultiAlternatives (
+				subject='New Meeting Details with Felix Hall',
+				body=msg,
+				from_email='info@felix-hall.com',
+				to=list([query_data['email']]),
+				bcc=list(['felix.p.hall@gmail.com']),
+				reply_to=list(['felix.p.hall@gmail.com']),
+				headers={'Content-Type': 'text/plain'})
+			email.send()
 
 		return self.update(request, *args, **kwargs)
 #https://stackoverflow.com/questions/31880227/django-rest-framework-method-put-not-allowed-in-viewset-with-def-update
--- wagtail_freelancer/settings/base.py
+++ wagtail_freelancer/settings/base.py
@@ -216,3 +216,17 @@ DEFAULT_FILE_STORAGE = 'wagtail_freelancer.storage_backends.PublicMediaStorage'
 
 AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
 PRIVATE_FILE_STORAGE = 'wagtail_freelancer.storage_backends.PrivateMediaStorage'
+
+#finish setting up email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
