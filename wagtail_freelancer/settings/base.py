03bda1e80602200e718e25b5882e1270cd0edafc
Felix <felix.p.hall@gmail.com>
2020-03-28 14:19:32 +1000

    uncommented AWS

--- wagtail_freelancer/settings/base.py
+++ wagtail_freelancer/settings/base.py
@@ -206,25 +206,25 @@ DATABASES['default']['CONN_MAX_AGE'] = 500
 
 
 
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = 'personal-portfolio-static-aws'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'personal-portfolio-static-aws'
 
-# AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = 'public-read'
 
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
-# AWS_STATIC_LOCATION = 'static'
-# STATICFILES_STORAGE = 'wagtail_freelancer.storage_backends.StaticStorage'
-# #STATIC_URL = '/static/'
-# STATIC_URL = config(STATIC_URL, "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION))
+AWS_STATIC_LOCATION = 'static'
+STATICFILES_STORAGE = 'wagtail_freelancer.storage_backends.StaticStorage'
+#STATIC_URL = '/static/'
+STATIC_URL = config(STATIC_URL, "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_STATIC_LOCATION))
 
-# AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
-# DEFAULT_FILE_STORAGE = 'wagtail_freelancer.storage_backends.PublicMediaStorage'
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+DEFAULT_FILE_STORAGE = 'wagtail_freelancer.storage_backends.PublicMediaStorage'
 
 # AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
 # PRIVATE_FILE_STORAGE = 'wagtail_freelancer.storage_backends.PrivateMediaStorage'
