3cdc98f1a75c90b239c163cee131960372d4197c
Felix <felix.p.hall@gmail.com>
2020-04-06 20:10:34 +1000

    added blan k and null to referee field

--- wagtail_freelancer/course/models.py
+++ wagtail_freelancer/course/models.py
@@ -9,7 +9,7 @@ from django.core.validators import RegexValidator, MinValueValidator
 # Create your models here.
 
 class HolidayCourseInterestData(models.Model):
-	referee = models.CharField(max_length=200, default='')
+	referee = models.CharField(max_length=200, default='', null=True, blank=True)
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	student_keen = models.BooleanField(default=True)
